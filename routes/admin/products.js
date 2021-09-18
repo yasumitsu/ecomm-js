6da123e3e275d017a475dd858cef6d26f40318d6
Kenji <kenjiyasumitsu@gmail.com>
2021-09-19 07:54:51 +0900

    added upload images route

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,29 +1,35 @@
 const express = require('express');
-const { validationResult } = require('express-validator');
 const multer = require('multer');
 
+const { handleErrors } = require('./middlewares');
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
+const productsIndexTemplate = require('../../views/admin/products/index');
 const { requireTitle, requirePrice } = require('./validators');
 
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
-router.get('/admin/products', (req, res) => {});
+router.get('/admin/products', async (req, res) => {
+	const products = await productsRepo.getAll();
+	res.send(productsIndexTemplate({ products }));
+});
 
 router.get('/admin/products/new', (req, res) => {
 	res.send(productsNewTemplate({}));
 });
 
-router.post('/admin/products/new', upload.single('image'), [ requireTitle, requirePrice ], async (req, res) => {
-	const errors = validationResult(req);
-
-	if (!errors.isEmpty()) return res.send(productsNewTemplate({ errors }));
-
-	const image = req.file.buffer.toString('base64');
-	const { title, price } = req.body;
-	await productsRepo.create({ title, price, image });
-	res.send('submitted');
-});
+router.post(
+	'/admin/products/new',
+	upload.single('image'),
+	[ requireTitle, requirePrice ],
+	handleErrors(productsNewTemplate),
+	async (req, res) => {
+		const image = req.file.buffer.toString('base64');
+		const { title, price } = req.body;
+		await productsRepo.create({ title, price, image });
+		res.redirect('/admin/products');
+	}
+);
 
 module.exports = router;
