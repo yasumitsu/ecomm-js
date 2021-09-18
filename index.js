4ecd268f213f24ec6e665fbeb76e2ee8a6c9f29d
Kenji <kenjiyasumitsu@gmail.com>
2021-09-18 12:36:02 +0900

    added products admin page

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
+const productsRouter = require('./routes/admin/products');
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.use(
 );
 
 app.use(authRouter);
+app.use(productsRouter);
 
 app.listen(3000, () => {
 	console.log('listening at port 3000');
---
+++ routes/admin/products.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const { validationResult } = require('express-validator');
+const productsRepo = require('../../repositories/products');
+const productsNewTemplate = require('../../views/admin/products/new');
+const { requireTitle, requirePrice } = require('./validators');
+
+const router = express.Router();
+
+router.get('/admin/products', (req, res) => {});
+
+router.get('/admin/products/new', (req, res) => {
+	res.send(productsNewTemplate({}));
+});
+
+router.post('/admin/products/new', [ requireTitle, requirePrice ], (req, res) => {
+	const errors = validationResult(req);
+	res.send('submitted');
+});
+
+module.exports = router;
